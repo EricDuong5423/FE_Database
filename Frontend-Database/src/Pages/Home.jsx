63bf4887b66313825eba189d20bd58bf92ac3663
EricDuong5423 <tuduong05042003@gmail.com>
2024-12-02 09:56:49 +0700

    Upload from Mac

--- Frontend-Database/src/Pages/Home.jsx
+++ Frontend-Database/src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import SlideMenu from "../Components/SlideMenuCustomer";
 import "../Styles/HomePage.css";
+import pizzaImage from "../../public/pizzaAI.png";
 
 function HomePage() {
   return (
     <>
       <SlideMenu />
       <TodaySales />
+      <img src={pizzaImage} width="100%" />
+      <IngredientsDescription />
     </>
   );
 }
@@ -15,6 +18,14 @@ function TodaySales() {
     <div className="today-sales">
       <h3>Today's top sales!</h3>
       <p>let's take a look at our best seller!</p>
+    </div>
+  );
+}
+
+function IngredientsDescription() {
+  return (
+    <div className="ingredients-description">
+      <h3>gifts from mother of nature</h3>
     </div>
   );
 }
